5a7b5be801a10076534536e190f82224917de86a
Vignesh Bharathi <vigneshbharathi68@hotmail.com>
2025-12-03 19:17:08 +0530

    Initial commit

---
+++ src/main/java/com/vignesh/spring_data_jpa_ex/SpringDataJpaExApplication.java
@@ -0,0 +1,13 @@
+package com.vignesh.spring_data_jpa_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaExApplication.class, args);
+	}
+
+}
