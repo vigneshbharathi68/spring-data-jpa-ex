75e2cf1f463b7c6418fb87838a88b2f91d5465c7
Vignesh Bharathi <vigneshbharathi68@hotmail.com>
2025-12-04 13:27:11 +0530

    Repository created for creating table and adding data to table

--- src/main/java/com/vignesh/spring_data_jpa_ex/SpringDataJpaExApplication.java
+++ src/main/java/com/vignesh/spring_data_jpa_ex/SpringDataJpaExApplication.java
@@ -2,12 +2,28 @@ package com.vignesh.spring_data_jpa_ex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.vignesh.spring_data_jpa_ex.model.Student;
 
 @SpringBootApplication
 public class SpringDataJpaExApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJpaExApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringDataJpaExApplication.class, args);
+
+		StudentRepo repo = context.getBean(StudentRepo.class);
+
+		Student s1 = context.getBean(Student.class);
+		// Student s2 = context.getBean(Student.class);
+		// Student s3 = context.getBean(Student.class);
+
+		s1.setRollNo(101);
+		s1.setName("Vignesh");
+		s1.setMarks(87);
+
+		repo.save(s1);
+
 	}
 
-}
+} 
---
+++ src/main/java/com/vignesh/spring_data_jpa_ex/model/Student.java
@@ -0,0 +1,42 @@
+package com.vignesh.spring_data_jpa_ex.model;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Component
+@Scope("prototype")
+@Entity
+public class Student {
+  @Id
+  private int rollNo;
+  private String name;
+  private int marks;
+
+  public int getRollNo() {
+    return rollNo;
+  }
+
+  public void setRollNo(int rollNo) {
+    this.rollNo = rollNo;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getMarks() {
+    return marks;
+  }
+
+  public void setMarks(int marks) {
+    this.marks = marks;
+  }
+
+}
