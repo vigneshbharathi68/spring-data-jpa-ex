00b59eb9c300d1cd03745a38155d40462c04b782
Vignesh Bharathi <vigneshbharathi68@hotmail.com>
2025-12-04 18:00:15 +0530

    UPdate and delete in JPA finished

--- src/main/java/com/vignesh/spring_data_jpa_ex/SpringDataJpaExApplication.java
+++ src/main/java/com/vignesh/spring_data_jpa_ex/SpringDataJpaExApplication.java
@@ -12,13 +12,13 @@ public class SpringDataJpaExApplication {
 
 		StudentRepo repo = context.getBean(StudentRepo.class);
 
-		// Student s1 = context.getBean(Student.class);
+		Student s1 = context.getBean(Student.class);
 		// Student s2 = context.getBean(Student.class);
 		// Student s3 = context.getBean(Student.class);
 
-		// s1.setRollNo(101);
-		// s1.setName("Vignesh");
-		// s1.setMarks(87);
+		s1.setRollNo(101);
+		s1.setName("Vignesh");
+		s1.setMarks(95);
 
 		// s2.setRollNo(102);
 		// s2.setName("Navid");
@@ -28,13 +28,16 @@ public class SpringDataJpaExApplication {
 		// s3.setName("Reddy");
 		// s3.setMarks(87);
 
-		// repo.save(s1);
+		repo.save(s1);
+		// repo.delete(s2);
 
 		// Optional<Student> s= repo.findById(101);
 
 		// s.ifPresent(System.out::print);
 
 		System.out.println(repo.findByName("Vignesh"));
+
+
 
 	}
 
